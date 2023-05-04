a97363df19a226c79e39f8270bbf54bd806c916b
Umar Baymran <1000slow@gmail.com>
2023-05-04 15:21:53 +0300

    angular material removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
-import {MatTooltipModule} from "@angular/material/tooltip";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -17,12 +12,7 @@ import {MatButtonModule} from "@angular/material/button";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatSlideToggleModule,
-    MatTooltipModule,
-    MatIconModule,
-    MatButtonModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
